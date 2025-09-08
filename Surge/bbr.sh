b44520e30cf1fb009f9bc3e49431a7b167fe0965
sealszzz <55393680+sealszzz@users.noreply.github.com>
2025-09-08 14:01:52 +0800

    Update bbr.sh

--- Surge/bbr.sh
+++ Surge/bbr.sh
@@ -16,31 +16,8 @@ configure_system_and_bbr() {
     echo -e "${YELLOW}配置系统参数和BBR...${RESET}"
     
     cat > /etc/sysctl.conf << EOF
-fs.file-max = 6815744
-net.ipv4.tcp_no_metrics_save = 1
-net.ipv4.tcp_ecn = 0
-net.ipv4.tcp_frto = 0
-net.ipv4.tcp_mtu_probing = 0
-net.ipv4.tcp_rfc1337 = 0
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_fack = 1
-net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_adv_win_scale = 1
-net.ipv4.tcp_moderate_rcvbuf = 1
-net.core.rmem_max = 33554432
-net.core.wmem_max = 33554432
-net.ipv4.tcp_rmem = 4096 87380 33554432
-net.ipv4.tcp_wmem = 4096 16384 33554432
-net.ipv4.udp_rmem_min = 8192
-net.ipv4.udp_wmem_min = 8192
-net.ipv4.ip_forward = 1
-net.ipv4.conf.all.route_localnet = 1
-net.ipv4.conf.all.forwarding = 1
-net.ipv4.conf.default.forwarding = 1
 net.core.default_qdisc = fq_pie
 net.ipv4.tcp_congestion_control = bbr
-net.ipv6.conf.all.forwarding = 1
-net.ipv6.conf.default.forwarding = 1
 EOF
 
     sysctl -p
