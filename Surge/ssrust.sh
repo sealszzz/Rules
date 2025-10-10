06e898c3d60cb5696a66d661876f79a238cde2b6
sealszzz <55393680+sealszzz@users.noreply.github.com>
2025-10-10 23:09:23 +0800

    Update ssrust.sh

--- Surge/ssrust.sh
+++ Surge/ssrust.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 #================= 脚本元信息（用于自升级） =================
-SCRIPT_VERSION="1.3.0"
+SCRIPT_VERSION="1.3.1"
 SCRIPT_INSTALL="/usr/local/sbin/ssrust.sh"
 SCRIPT_LAUNCHER="/usr/local/bin/ssrust"
 SCRIPT_REMOTE_RAW="https://raw.githubusercontent.com/sealszzz/Rules/refs/heads/master/Surge/ssrust.sh"
