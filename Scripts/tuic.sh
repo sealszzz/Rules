5e32037d6ffcaace7365ed48f819b036933e66a0
sealszzz <55393680+sealszzz@users.noreply.github.com>
2025-12-11 23:22:24 +0800

    Update tuic.sh

--- Scripts/tuic.sh
+++ Scripts/tuic.sh
@@ -72,7 +72,6 @@ udp_relay_ipv6 = true
 zero_rtt_handshake = false
 dual_stack = false
 auth_timeout = "8s"
-max_idle_time = "8s"
 
 [users]
 "${TUIC_UUID}" = "${TUIC_PASS}"
@@ -82,6 +81,9 @@ self_sign = false
 certificate = "${CERT}"
 private_key = "${KEY}"
 alpn = ["h3"]
+
+[quic]
+max_idle_time = "30s" 
 
 [quic.congestion_control]
 controller = "bbr"
